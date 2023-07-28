dc266703a3abf090ab7b2f8601996cb1c87ca093
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-07-28 14:31:42 +0200

    added students who are over 30 years old

--- db_university_queries.sql
+++ db_university_queries.sql
@@ -15,6 +15,10 @@ WHERE `cfu` >10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
+SELECT * 
+FROM `students`
+WHERE (2023 - YEAR(`date_of_birth`)>30);
+
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
