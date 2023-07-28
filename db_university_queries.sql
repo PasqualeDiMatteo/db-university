3bc3efed209392424a6afb0520fe06a5758939bc
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-07-28 15:00:20 +0200

    added enrolled per year

--- db_university_queries.sql
+++ db_university_queries.sql
@@ -54,6 +54,10 @@ WHERE `phone` IS NULL;
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 
+SELECT COUNT(*) AS 'Iscritti', YEAR(`enrolment_date`) AS 'anno'
+FROM `students`
+GROUP BY `anno`;
+
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
