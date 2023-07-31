36e14d3a262f3428646608d6b9d8afdaf90d1e87
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-07-31 14:25:25 +0200

    Added request 2 JOIN

--- db_university_queries.sql
+++ db_university_queries.sql
@@ -88,6 +88,12 @@ WHERE D.`name`= 'Corso di Laurea in Economia';
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
+SELECT DEG.`name`,DEG.`level`,DEG.`website`,DEG.`address`
+FROM `degrees` AS DEG
+JOIN `departments`AS DEP
+ON DEG.`department_id`= DEP.`id`
+WHERE DEP.`name`='Dipartimento di Neuroscienze';
+
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
 
